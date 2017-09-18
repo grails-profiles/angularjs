dd5419ababd7b8844b77bda8fa59e85e872ea0e0
sdelamo <sergio.delamo@softamo.com>
2017-09-18 09:30:24 +0200

    Generate docs if not a pull request

--- travis-build.sh
+++ travis-build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 set -e
+./gradlew clean
+EXIT_STATUS=0
+echo "Publishing docs for branch $TRAVIS_BRANCH"
+if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
+    echo "Publishing docs"
+    ./gradlew docs || EXIT_STATUS=$?
+fi
+
+exit $EXIT_STATUS
